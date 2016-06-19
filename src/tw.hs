eb204e9e55ed7f285b443aea66e73241ad935fcb
Daniel Lovasko <lovasko@freebsd.org>
2016-06-19 15:13:14 +0100

    converting strings to lazy text

--- src/tw.hs
+++ src/tw.hs
@@ -1,14 +1,15 @@
-import Control.Monad
 import Data.Bifunctor
 import Data.Char
 import Data.Word
 import System.Environment
 import System.Exit
+import qualified Data.Text.Lazy as Text
+import qualified Data.Text.Lazy.IO as Text
 
 -- | Check whether a line ends with whitespace.
-check :: (Word64, String)
+check :: (Word64, Text.Text)
       -> (Word64, Bool)
-check = second (isSpace . last)
+check = second (isSpace . Text.last)
 
 -- | Print a warning to the stdout with the line number.
 warn :: (Word64, Bool)
@@ -17,10 +18,10 @@ warn (_, False)  = return ()
 warn (num, True) = print num
 
 -- | Parse command-line arguments.
-getInput :: [String]  -- ^ arguments
-         -> IO String -- ^ input file's content
-getInput []       = getContents
-getInput (path:_) = readFile path
+getInput :: [String]     -- ^ arguments
+         -> IO Text.Text -- ^ input file's content
+getInput []       = Text.getContents
+getInput (path:_) = Text.readFile path
 
 -- | Take a decision based on a boolean value.
 decide :: Bool -- ^ value
@@ -35,8 +36,8 @@ main :: IO ()
 main = do
   args <- getArgs
   input <- getInput args
-  let numbered = zip [1..] (lines input)
-  let nonEmpty = filter (not . null . snd) numbered
+  let numbered = zip [1..] (Text.lines input)
+  let nonEmpty = filter (not . Text.null . snd) numbered
   let trailing = map check nonEmpty
   mapM_ warn trailing
   decide (any snd trailing) exitFailure exitSuccess
