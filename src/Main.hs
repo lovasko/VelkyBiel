c56e944ebd31e569328ff5606924ed6caadaef69
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-10-27 02:07:30 +0100

    changed main file

--- src/Main.hs
+++ src/Main.hs
@@ -7,33 +7,33 @@ import Data.Word
 import System.Environment
 import System.Exit
 
-import qualified Data.Text.Lazy as Text
-import qualified Data.Text.Lazy.IO as Text
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as T
 
 -- | File entry defined by it's name and content.
-data Entry = Entry Text.Text Text.Text
+data Entry = Entry T.Text T.Text
 
 -- | Check whether a line ends with whitespace.
-check :: (Word64, Text.Text) -- ^ numbered line
+check :: (Word64, T.Text) -- ^ numbered line
       -> (Word64, Bool)      -- ^ numbered decision
-check = second (isSpace . Text.last)
+check = second (isSpace . T.last)
 
 -- | Print a warning to the stdout with the line number.
-warn :: Text.Text      -- ^ file path
+warn :: T.Text      -- ^ file path
      -> (Word64, Bool) -- ^ line decision
      -> IO ()          -- ^ action
 warn _    (_, False)  = return ()
-warn path (num, True) = Text.putStrLn $ path <> ": " <> Text.pack (show num)
+warn path (num, True) = T.putStrLn $ path <> ": " <> T.pack (show num)
 
 -- | Parse command-line arguments.
 getEntries :: [String]   -- ^ arguments
            -> IO [Entry] -- ^ file entries
 getEntries []    = do
-  content <- Text.getContents
+  content <- T.getContents
   return [Entry "<stdin>" content]
 getEntries paths = do
-  contents <- mapM Text.readFile paths
-  return $ zipWith Entry (map Text.pack paths) contents
+  contents <- mapM T.readFile paths
+  return $ zipWith Entry (map T.pack paths) contents
 
 -- | Take a decision based on a boolean value.
 decide :: Bool -- ^ value
@@ -48,8 +48,8 @@ decide False _ y = y
 processEntry :: Entry   -- ^ entry
              -> IO Bool -- ^ trailing decision
 processEntry (Entry path content) = do
-  let numbered = zip [1..] (Text.lines content)
-  let nonEmpty = filter (not . Text.null . snd) numbered
+  let numbered = zip [1..] (T.lines content)
+  let nonEmpty = filter (not . T.null . snd) numbered
   let trailing = map check nonEmpty
   mapM_ (warn path) trailing
   return $ any snd trailing
