7ae3d5dc8a26865c82db5220bc35de2abb5ddcc6
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-10-31 17:05:36 +0000

    change from Text to ByteString.Char8; simplification of algorithms

--- src/Main.hs
+++ src/Main.hs
@@ -1,63 +1,53 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-import Data.Bifunctor
 import Data.Char
-import Data.Monoid
-import Data.Word
 import System.Environment
 import System.Exit
 
-import qualified Data.Text.Lazy as T
-import qualified Data.Text.Lazy.IO as T
+import qualified Data.ByteString.Lazy.Char8 as C
 
--- | File entry defined by it's name and content.
-data Entry = Entry T.Text T.Text
+-- | File entry defined by its name and content.
+data Entry = Entry
+             String       -- ^ name
+             C.ByteString -- ^ content
 
 -- | Check whether a line ends with whitespace.
-check :: (Word64, T.Text) -- ^ numbered line
-      -> (Word64, Bool)      -- ^ numbered decision
-check = second (isSpace . T.last)
+check :: (Integer, C.ByteString) -- ^ number & line
+      -> (Integer, Bool)         -- ^ number & decision
+check (num, line) = (num, (isSpace . C.last) line)
 
 -- | Print a warning to the stdout with the line number.
-warn :: T.Text      -- ^ file path
-     -> (Word64, Bool) -- ^ line decision
-     -> IO ()          -- ^ action
-warn _    (_, False)  = return ()
-warn path (num, True) = T.putStrLn $ path <> ": " <> T.pack (show num)
+warn :: String          -- ^ file path
+     -> (Integer, Bool) -- ^ line decision
+     -> IO ()           -- ^ action
+warn _    (_,   False)  = return ()
+warn path (num, True)   = putStrLn $ path ++ ": " ++ show num
 
 -- | Parse command-line arguments.
 getEntries :: [String]   -- ^ arguments
            -> IO [Entry] -- ^ file entries
 getEntries []    = do
-  content <- T.getContents
+  content <- C.getContents
   return [Entry "<stdin>" content]
 getEntries paths = do
-  contents <- mapM T.readFile paths
-  return $ zipWith Entry (map T.pack paths) contents
-
--- | Take a decision based on a boolean value.
-decide :: Bool -- ^ value
-       -> a    -- ^ True option
-       -> a    -- ^ False option
-       -> a    -- ^ decision
-decide True  x _ = x
-decide False _ y = y
+  contents <- mapM C.readFile paths
+  return $ zipWith Entry paths contents
 
 -- | Process a single file entry - determine the trailing whitespace
 -- property and print an appropriate warning to the stdout.
 processEntry :: Entry   -- ^ entry
              -> IO Bool -- ^ trailing decision
 processEntry (Entry path content) = do
-  let numbered = zip [1..] (T.lines content)
-  let nonEmpty = filter (not . T.null . snd) numbered
+  let numbered = zip [1..] (C.lines content)
+  let nonEmpty = filter (not . C.null . snd) numbered
   let trailing = map check nonEmpty
   mapM_ (warn path) trailing
   return $ any snd trailing
 
--- | Print numbers of lines that end with whitespace.
+-- | Print numbers of lines and corresponding filenames that contain
+-- trailing whitespace. The utility expects zero or more files as its
+-- arguments. In case of zero arguments, the stdin stream is used.
 main :: IO ()
 main = do
   entries  <- getArgs >>= getEntries
   trailing <- mapM processEntry entries
-  decide (or trailing) exitFailure exitSuccess
+  if or trailing then exitFailure else exitSuccess
 
